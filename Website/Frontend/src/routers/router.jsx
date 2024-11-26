ae759e4b4d68b064c2234d937a5ba0d0e6d3ae99
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2024-11-27 03:00:33 +0700

    Adjusted the router
    - Changed the path to the Login.jsx
    - Added path to LoginUser.jsx
    - Added LoginUser to path '/login-user'

--- Website/Frontend/src/routers/router.jsx
+++ Website/Frontend/src/routers/router.jsx
@@ -1,47 +1,52 @@
-import { createBrowserRouter,} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import UploadFiles from "../pages/UploadFiles";
 import History from "../pages/History";
 import Buy from "../pages/Buy";
-import Login from "../pages/Login";
+import Login from "../pages/Login/Login";
+import LoginUser from "../pages/Login/LoginUser";
 import Dashboard from "../pages/Dashboard";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
         path: "/",
-        element: <App/>,
-        children: [
-            {
-                path: '/',
-                element: <Home/>
-            },
-            {
-                path: '/print',
-                element: <UploadFiles />
-            },
-            {
-                path: '/history',
-                element: <History />
-            },
-            {
-                path: '/buy',
-                element: <Buy />
-            },
-            {
-                path: '/dashboard',
-                element: <Dashboard />
-            },
-        ]
-    },
-    {
-        path: '/signup',
-        element: <Login />
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
+        element: <Home />,
+      },
+      {
+        path: "/print",
+        element: <UploadFiles />,
+      },
+      {
+        path: "/history",
+        element: <History />,
+      },
+      {
+        path: "/buy",
+        element: <Buy />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/signup",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/login-user",
+    element: <LoginUser />,
+  },
 ]);
 
 export default router;
