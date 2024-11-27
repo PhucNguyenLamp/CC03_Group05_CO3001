29b88230d3258a8cb27c83458bdd83e525c78317
Gia Phuc <giaphuc29082004@gmail.com>
2024-11-27 21:25:22 +0700

    update require dotenv

--- Website/Server/config/db.config.js
+++ Website/Server/config/db.config.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
--- Website/Server/server.js
+++ Website/Server/server.js
@@ -1,7 +1,6 @@
 import app from '../Server/app.js'
 import dbconnection from './config/db.config.js';
 
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
