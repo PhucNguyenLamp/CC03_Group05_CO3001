df4c9dc3c13a715c5564ca0f0abcd3ec5bc4d227
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2024-11-29 20:52:34 +0700

    Modifed AppBar to match Figma design

--- Website/Frontend/src/components/Appbar.jsx
+++ Website/Frontend/src/components/Appbar.jsx
@@ -12,9 +12,11 @@ import {
   MenuItem,
 } from "@mui/material";
 import { PrintTwoTone } from "@mui/icons-material";
-import { useNavigate } from "react-router";
-import Dashboard from './../pages/Dashboard';
+import { useLocation, useNavigate } from "react-router";
+import Dashboard from "./../pages/Dashboard";
 import { useAuth } from "../contexts/AuthContext";
+import Grid from "@mui/material/Grid2";
+import hcmut_logo from "../assets/HCMUT.png";
 
 const pages = [
   { name: "In tài liệu", path: "/print" },
@@ -30,9 +32,10 @@ const settings = [
 export default function Appbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const navigate = useNavigate();
   const { logout, userInfo } = useAuth();
   const [isCustomer] = useState(true);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -52,45 +55,87 @@ export default function Appbar() {
 
   const handleLogOut = () => {
     logout();
-    navigate('/login');
-  }
+    navigate("/login");
+  };
   return (
-    <AppBar position="sticky" >
-      <Toolbar className="flex justify-between items-center px-6">
-        {/* Logo and Title */}
+    <AppBar position="sticky" sx={{ backgroundColor: "rgba(255,255,255,0.2)" }}>
+      <Toolbar
+        className="flex justify-between items-center px-6"
+        sx={{
+          display: "display",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingX: 2,
+        }}>
         <Box
-          className="flex items-center space-x-3"
-          onClick={() => navigate("/")}>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <PrintTwoTone />
-          </IconButton>
-          <Typography variant="h6" component="div">
-            Bách Khoa Printing
-          </Typography>
-        </Box>
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+          {/* Logo and Title */}
+          <Box
+            component="img"
+            src={hcmut_logo}
+            alt="HCMUT LOGO"
+            sx={{
+              height: 50,
+              cursor: "pointer",
+              marginLeft: 3,
+              marginRight: 5,
+            }}
+            onClick={() => navigate("/")}
+          />
 
-        {/* Navigation Buttons */}
-        <Box className="md:flex space-x-4">
-          {pages.map((page, index) => (
-            <Button
-              key={index}
-              className="text-white py-2 px-4"
-              onClick={() => navigate(page.path)}>
-              {page.name}
-            </Button>
-          ))}
+          {/* Navigation Buttons */}
+          <Box sx={{ display: "flex", gap: 5 }}>
+            {pages.map((page, index) => (
+              <Button
+                key={index}
+                variant="text"
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: "18px",
+                  color: "primary.dark",
+                  backgroundColor: "transparent",
+                  textTransform: "none",
+                  borderRadius: "16px",
+                  paddingX: "24px",
+                  paddingY: "2px",
+                  border:
+                    location.pathname == page.path
+                      ? "2px solid"
+                      : "2px solid transparent",
+                  "&:hover": {
+                    border: "2px solid",
+                    borderColor: "primary.dark",
+                  },
+                }}
+                onClick={() => navigate(page.path)}>
+                {page.name}
+              </Button>
+            ))}
+          </Box>
         </Box>
 
         {/* User Menu */}
         {userInfo ? (
-          <Box sx={{display: 'flex', flexDirection: 'row', gap: 2, alignItems: 'center'}}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 2,
+              alignItems: "center",
+              marginRight: 3,
+            }}>
             <Typography>{userInfo.name}</Typography>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} className="p-0">
                 <Avatar alt="User" />
               </IconButton>
             </Tooltip>
-            
+
             <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
@@ -110,21 +155,29 @@ export default function Appbar() {
                 key={"dashboard"}
                 onClick={() => {
                   handleCloseUserMenu();
-                  navigate('/dashboard');
+                  navigate("/dashboard");
                 }}>
-                <Typography sx={{ textAlign: "center" }}>
-                  Dashboard
-                </Typography>
+                <Typography sx={{ textAlign: "center" }}>Dashboard</Typography>
               </MenuItem>
-              <MenuItem
-                key={"logout"}
-                onClick={handleLogOut}>
-                <Typography sx={{ textAlign: "center" }}>
-                  Log out
-                </Typography>
+              <MenuItem key={"logout"} onClick={handleLogOut}>
+                <Typography sx={{ textAlign: "center" }}>Log out</Typography>
               </MenuItem>
             </Menu>
-          </Box>) : (<Box><Button sx={{color: 'white'}} onClick={() => navigate('/login')}>Log in</Button></Box>)}
+          </Box>
+        ) : (
+          <Button
+            variant="contained"
+            sx={{
+              color: "white",
+              backgroundColor: "primary.main",
+              borderRadius: "14px",
+              marginRight: 3,
+              "&:hover": { backgroundColor: "primary.dark", color: "white" },
+            }}
+            onClick={() => navigate("/login")}>
+            Đăng nhập
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
