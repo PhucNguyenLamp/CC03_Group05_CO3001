4b8825a115cf9bd7a8bd74c1f45e4a1845d90c92
Gia Phuc <giaphuc29082004@gmail.com>
2024-12-01 13:45:30 +0700

    fix date in server

--- Website/Server/config/db.config.js
+++ Website/Server/config/db.config.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
-
+dotenv.config();
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 const dbconnection = mongoose.connect(DB,{
--- Website/Server/utils/jwtToken.js
+++ Website/Server/utils/jwtToken.js
@@ -1,8 +1,10 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const signToken = (user,statusCode,res) => {
   const token= jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET,{
-    expiresIn:Number(process.env.JWT_EXPIRES)*3600
+    expiresIn: Number(process.env.JWT_EXPIRES)*3600*1000
   });
   user.password=undefined;
   res.status(statusCode).json({
